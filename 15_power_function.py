6925428fd64abf04c959cfcd84d89a4b826f182f
XiaMiLang <n97071159@gs.ncku.edu.tw>
2019-10-05 16:11:42 +0800

    20191005

---
+++ 15_power_function.py
@@ -0,0 +1,11 @@
+#exponent function 指數
+#print(2**3)         #等於 2^3
+
+# def raise_to_power(base, power):
+#     result = 1
+#     for index in range(power):
+#         result *=base
+#     print(result)
+# raise_to_power(2,3)
+
+print(pow(2, 3))
